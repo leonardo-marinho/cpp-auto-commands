ce6316cae80e391a0247127bc46a1c0b1189a34c
leonardo-marinho <leonardo.msnt@outlook.com>
2023-04-24 20:47:04 -0300

    Added config structures

---
+++ lib/Config.hpp
@@ -0,0 +1,35 @@
+#ifndef Config_hpp
+#define Config_hpp
+
+struct OTAConfig
+{
+  char *otaHostname;
+  char *otaPassword;
+  char *wifiHostname;
+  char *wifiPassword;
+};
+
+struct AutoCommandPedalConfig
+{
+  int buttonPin;
+  int ledPin;
+  int outputPin;
+};
+
+struct AutoCommandGateConfig
+{
+  int buttonPin;
+  int ledPin;
+  int outputPin1;
+  int outputPin2;
+};
+
+struct Config
+{
+  OTAConfig otaConfig;
+  AutoCommandPedalConfig autoCommandPedal;
+  AutoCommandGateConfig autoCommandGate;
+  int backlightPin;
+};
+
+#endif
